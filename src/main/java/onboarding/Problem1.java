8c676ce09377fdbd7034a86973bff5c574ab78f8
tam31 <wxogud3377@naver.com>
2023-09-12 16:19:02 +0900

    [1번문제] feat: 데이터 유효성 검사

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -10,6 +10,31 @@ class Problem1 {
 
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int ans = 0;
+
+        if(isNotSize(pobi) || isNotSidePage(pobi) || isNotRange(pobi)){
+            return exceptionNum;
+        }
+
+        if(isNotSize(crong) || isNotSidePage(crong) || isNotRange(crong)){
+            return exceptionNum;
+        }
+
         return ans;
+    }
+
+    private static boolean isNotSize(List<Integer> checkList){
+        return checkList.size() !=2;
+    }
+
+    private static boolean isNotSidePage(List<Integer> checkList){
+        return checkList.get(1) - checkList.get(0) != 1;
+    }
+
+    private static boolean isNotRange(List<Integer> checkList){
+        if((checkList.get(0)>0 && checkList.get(0)<=400)
+        && (checkList.get(1)>0 && checkList.get(1)<=400)){
+            return false;
+        }
+        return true;
     }
 }
