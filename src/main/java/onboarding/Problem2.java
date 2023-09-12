32884a7fbbc38c9b749569e5000d57a50cef8602
tam31 <wxogud3377@naver.com>
2023-09-12 18:20:16 +0900

    [2번문제] feat: 결과별 상수 초기화

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,10 @@
 package onboarding;
 
 public class Problem2 {
+    private static boolean status = true;
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
         return answer;
     }
 }
