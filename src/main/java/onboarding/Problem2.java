77ea15f42f247d27e23e0c7e5897aa5ea0ebadfa
tam31 <wxogud3377@naver.com>
2023-09-13 02:40:43 +0900

    [2번문제] feat: 문자를 비교 후 중복되는 문자제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,10 +1,40 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     private static boolean status = true;
     public static String solution(String cryptogram) {
         String answer = "";
 
+        while(status){
+            status = false;
+            answer = getAnswerWithoutDuplicate(cryptogram);
+            cryptogram = answer;
+        }
         return answer;
+    }
+
+    private static String getAnswerWithoutDuplicate(String cryptogram){
+        if(cryptogram.length()==0) return "";
+
+        String removeDuplicate = "";
+        char checkDuplicateValue = cryptogram.charAt(0);
+        boolean checkDuplicateStatus = false;
+        for(int i=1; i<cryptogram.length(); i++){
+            if(checkDuplicateValue==cryptogram.charAt(i)){
+                checkDuplicateStatus = true;
+            }else if(checkDuplicateStatus && checkDuplicateValue != cryptogram.charAt(i)){
+                checkDuplicateValue = cryptogram.charAt(i);
+                checkDuplicateStatus = false;
+            }else{
+                removeDuplicate += checkDuplicateValue;
+                checkDuplicateValue = cryptogram.charAt(i);
+            }
+        }
+        if(!checkDuplicateStatus){
+            removeDuplicate += checkDuplicateValue;
+        }
+        return removeDuplicate;
     }
 }
