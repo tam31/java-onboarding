5e3f2547c4c7e9d4a4647b58ccd4a141734f0cd6
tam31 <wxogud3377@naver.com>
2023-09-12 15:54:02 +0900

    [1번 문제] feat: 결과 별 상수 초기화

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,13 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    private static final int exceptionNum = -1;
+    private static final int compareNum = 0;
+    private static final int winPobiNum = 1;
+    private static final int winCrongNum = 2;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        int ans = 0;
+        return ans;
     }
 }
