5c12119e12f52fe51aa761f261803eed61c826c0
tam31 <wxogud3377@naver.com>
2023-09-13 15:38:44 +0900

    [3번문제] feat: 상수 초기값 설정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,11 @@
 package onboarding;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Problem3 {
+    private static final Set<Character> checkContain369 = new HashSet<>(List.of('3','6','9'));
     public static int solution(int number) {
         int answer = 0;
         return answer;
