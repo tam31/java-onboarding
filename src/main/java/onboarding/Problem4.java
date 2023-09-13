a02d647b106e63b98118a9dff1ff73ec9222e921
tam31 <wxogud3377@naver.com>
2023-09-13 17:47:31 +0900

    [4번문제] feat: 소문자 청개구리화법  변환 a + (z-현재문자의 아스크코드)

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,9 +3,33 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        answer = getGreenFrog(word);
         return answer;
     }
-    
+
+    private static String getGreenFrog(String word){
+        String greenFrog = "";
+        for(int i=0; i<word.length(); i++){
+            greenFrog += Character.isAlphabetic(word.charAt(i)) ?
+                    checkAlpahetChar(word.charAt(i)) : word.charAt(i);
+        }
+        return greenFrog;
+    }
+    private static char checkAlpahetChar(char word){
+        if(checkLowerAlpahet(word)){
+            return changeLowerAlpahet(word);
+        }
+        return changeUpperAlpahet(word);
+    }
+
+    private static char changeLowerAlpahet(char word){
+        return (char)('a' + ('z'-word));
+    }
+
+    private static char changeUpperAlpahet(char word){
+       
+    }
+
     private static boolean checkLowerAlpahet(char word){
         return Character.isLowerCase(word);
     }
