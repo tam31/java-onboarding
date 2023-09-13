351f9cff2c097a8511c7d28be4a32fb4b5941f8f
tam31 <wxogud3377@naver.com>
2023-09-13 15:41:55 +0900

    [3번문제] feat: 1~number까지 3,6,9가 포함되는 개수 리턴 1~number까지의 수를 문자열로 바꾸어 3,6,9가 포함되어 있으면 count+1

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -8,6 +8,19 @@ public class Problem3 {
     private static final Set<Character> checkContain369 = new HashSet<>(List.of('3','6','9'));
     public static int solution(int number) {
         int answer = 0;
+        for(int i=1; i<=number; i++){
+            String changeNumber = String.valueOf(i);
+            answer += getAnsewerContain369(changeNumber);
+        }
         return answer;
+    }
+    private static int getAnsewerContain369(String changeNumber){
+        int count = 0;
+        for(int i=0; i<changeNumber.length(); i++){
+            if(checkContain369.contains(changeNumber.charAt(i))){
+                count +=1;
+            }
+        }
+        return count;
     }
 }
