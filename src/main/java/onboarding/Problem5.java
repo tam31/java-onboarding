9359b0f1f29c83d03a63a747e8d3376e4def9cd9
tam31 <wxogud3377@naver.com>
2023-09-14 12:54:51 +0900

    [5번문제] feat: 5만원~1원까지의 금액별 개수

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,6 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,7 +8,17 @@ public class Problem5 {
 
     private static final int[] listMoney = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(Collections.emptyList());
+
+        for(int i=0; i<listMoney.length; i++){
+            int getMoney = getAnswerMoney(money, listMoney[i]);
+            answer.add(getMoney);
+        }
         return answer;
     }
+
+    private static int getAnswerMoney(int money, int listMoney){
+        return money/listMoney;
+    }
+
 }
