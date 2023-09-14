249ec63161ab7cb67b24c6746a85cdd8556c8206
tam31 <wxogud3377@naver.com>
2023-09-14 15:57:14 +0900

    [7번문제] feat: 사용자의 친구와 사용자의 친구의 친구 변수 설정

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,13 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
+
+    private static Set<String> userFriendList = new HashSet<>();
+    private static Map<String, Integer> recommendFriendList = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
+        List<String> answer = new ArrayList<>(Collections.emptyList());
         return answer;
     }
 }
