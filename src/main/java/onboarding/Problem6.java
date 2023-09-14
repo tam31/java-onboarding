7a4a4f55cc09dbc9113b18942e0a27e377603e1f
tam31 <wxogud3377@naver.com>
2023-09-14 13:39:37 +0900

    [6번문제] feat: 중복확인 초기값 설정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,13 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
+
+    private static HashMap<String, String> checkDuplication = new HashMap<>();
+    private static Set<String> duplicationEmail = new HashSet<>();
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
         return answer;
     }
 }
