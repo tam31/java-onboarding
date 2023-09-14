9baa722106d39248750791250b5ce654548c43d1
tam31 <wxogud3377@naver.com>
2023-09-14 16:05:20 +0900

    [7번문제] feat: friends속 사용자의 친구가 있는지 확인

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -8,6 +8,19 @@ public class Problem7 {
     private static Map<String, Integer> recommendFriendList = new HashMap<>();
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>(Collections.emptyList());
+
+        for(List<String> friend: friends){
+            String userA = friend.get(0);
+            String userB = friend.get(1);
+            if(checkUserFriend(userA, userB, user)){
+
+            }
+        }
+
         return answer;
+    }
+
+    private static boolean checkUserFriend(String userA, String userB, String user){
+        return user.equals(userA) || user.equals(userB);
     }
 }
