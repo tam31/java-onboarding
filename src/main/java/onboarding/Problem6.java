494ccaf81407b444d600c441605a7c641497c920
tam31 <wxogud3377@naver.com>
2023-09-15 14:04:08 +0900

    [6번 문제] Style: getAnswerCheckDuplications 메소드 이름 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -12,14 +12,14 @@ public class Problem6 {
         for(List<String> list: forms){
             String email = list.get(0);
             String name = list.get(1);
-            getAnswerCheckDulications(name, email);
+            getAnswerCheckDuplications(name, email);
         }
         answer = new ArrayList<>(duplicationEmail);
         Collections.sort(answer);
         return answer;
     }
 
-    private static void getAnswerCheckDulications(String name, String email){
+    private static void getAnswerCheckDuplications(String name, String email){
         for(int i=0; i<=name.length()-2; i++){
             String checkName = name.substring(i,i+2);
             if(checkDuplication.containsKey(checkName)){
@@ -30,4 +30,5 @@ public class Problem6 {
             }
         }
     }
+
 }
